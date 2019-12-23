8355a4017806940a85bc32eea7896bbf600ac8dc
ang <angiezafra07@gmail.com>
2019-12-22 21:30:04 -0500

    adding buttons /some small css

---
+++ my-app/src/components/MusicDiv.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+export default class MusicDiv extends Component {
+  render() {
+    return (
+      <div id="main-div">
+
+        <button>
+      Boop
+        </button>
+        <button>
+      Boop
+        </button>
+        <button>
+      Boop
+        </button>
+        <button>
+      Boop
+        </button>
+        <button>
+      Boop
+        </button>
+        <button>
+      Boop
+        </button>
+
+
+
+      </div>
+    )
+  }
+}
